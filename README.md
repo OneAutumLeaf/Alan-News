3b480288b65d25edca3209c7d1851a10e0ac88cf
OneAutumLeafe <ayushsingh12072000@gmaile.com>
2022-11-07 22:00:16 +0530

    New_Commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# Alan-News
+# Voice Controlled React News Application - Alan AI Voice Assistant
+
+
+## Introduction
+  
+
+Setup:
+- run ```npm i && npm start``` to start development server
---
+++ src/App.js
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from 'react';
+import { Typography } from '@material-ui/core';
+import wordsToNumbers from 'words-to-numbers';
+import alanBtn from '@alan-ai/alan-sdk-web';
+
+import { NewsCards, Modal } from './components';
+import useStyles from './styles';
+
+const App = () => {
+  const [activeArticle, setActiveArticle] = useState(0);
+  const [newsArticles, setNewsArticles] = useState([]);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const classes = useStyles();
+
+  useEffect(() => {
+    alanBtn({
+      key: '58b558833e11d2704592939439dce15a2e956eca572e1d8b807a3e2338fdd0dc/stage',
+      onCommand: ({ command, articles, number }) => {
+        if (command === 'newHeadlines') {
+          setNewsArticles(articles);
+          setActiveArticle(-1);
+        } else if (command === 'instructions') {
+          setIsOpen(true);
+        } else if (command === 'highlight') {
+          setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
+        } else if (command === 'open') {
+          const parsedNumber = number.length > 2 ? wordsToNumbers((number), { fuzzy: true }) : number;
+          const article = articles[parsedNumber - 1];
+
+          if (parsedNumber > articles.length) {
+            alanBtn().playText('Please try that again...');
+          } else if (article) {
+            window.open(article.url, '_blank');
+            alanBtn().playText('Opening...');
+          } else {
+            alanBtn().playText('Please try that again...');
+          }
+        }
+      },
+    });
+  }, []);
+
+  return (
+    <div>
+      <div className={classes.logoContainer}>
+        {newsArticles.length ? (
+          <div className={classes.infoContainer}>
+            <div className={classes.card}><Typography variant="h5" component="h2">Try saying: <br /><br />Open article number [4]</Typography></div>
+            <div className={classes.card}><Typography variant="h5" component="h2">Try saying: <br /><br />Go back</Typography></div>
+          </div>
+        ) : null}
+        <img src="https://imgs.search.brave.com/sjOTqKepDMqyVY2y6M-8egN7hQ_wJtRKvAdxHNL1xK4/rs:fit:600:400:1/g:ce/aHR0cHM6Ly92b2lj/ZWJvdC5haS93cC1j/b250ZW50L3VwbG9h/ZHMvMjAxOS8xMC9h/bGFuLmpwZw" className={classes.alanLogo} alt="logo" />
+      </div>
+      <NewsCards articles={newsArticles} activeArticle={activeArticle} />
+      <Modal isOpen={isOpen} setIsOpen={setIsOpen} />
+      {!newsArticles.length ? (
+        <div className={classes.footer}>
+          <Typography variant="body1" component="h2">
+            Created by
+            <a className={classes.link} href="https://www.linkedin.com/in/ayush-singh-869149229/"> Ayush Singh</a> 
+          </Typography>
+        </div>
+      ) : null}
+    </div>
+  );
+};
+
+export default App;
